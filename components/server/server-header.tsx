2fc1f6fe74b09aca51abfb022c2bd820e8fff6c6
StrukovSergei <strukov.sergei.999@gmail.com>
2023-09-17 11:01:42 +0300

    Invitations

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -6,6 +6,7 @@ import { ServerWithMembersWithProfiles } from "@/types"
 import { MemberRole } from "@prisma/client"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { DropdownMenuItem } from "@radix-ui/react-dropdown-menu"
+import { useModal } from "@/hooks/use-modal-store"
 
 interface ServerHeaderProps {
     server: ServerWithMembersWithProfiles
@@ -16,6 +17,8 @@ export const ServerHeader = ({
     server,
     role
 }: ServerHeaderProps) => {
+    const { onOpen } = useModal()
+
     const isAdmin = role === MemberRole.ADMIN
     const isModerator = isAdmin || role === MemberRole.MODERATOR
 
@@ -32,7 +35,7 @@ export const ServerHeader = ({
                 </DropdownMenuTrigger>
                 <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
                     {isModerator && (
-                        <DropdownMenuItem className="focus:outline-none  hover:bg-zinc-700/10
+                        <DropdownMenuItem onClick={() => onOpen("invite", { server })} className="focus:outline-none  hover:bg-zinc-700/10
                         dark:hover:bg-zinc-700/50 transition text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer flex items-center">
                             Invite People
                             <UserPlus className="h-4 w-4 ml-auto" />
@@ -70,7 +73,7 @@ export const ServerHeader = ({
                         </DropdownMenuItem>
                     )}
                     {!isAdmin && (
-                        <DropdownMenuItem className="focus:outline-none  hover:bg-zinc-700/10
+                        <DropdownMenuItem className="text-rose-500 focus:outline-none  hover:bg-zinc-700/10
                         dark:hover:bg-zinc-700/50 transition px-3 py-2 text-sm cursor-pointer flex items-center">
                             Leave Server
                             <LogOut className="h-4 w-4 ml-auto" />

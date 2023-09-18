09f41664a02c664217177bfb07db0b5cf0b37116
StrukovSergei <strukov.sergei.999@gmail.com>
2023-09-18 11:04:40 +0300

    Delete & Leave server modal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -66,14 +66,14 @@ export const ServerHeader = ({
                         <DropdownMenuSeparator />
                     )}
                     {isAdmin && (
-                        <DropdownMenuItem className="text-rose-500 focus:outline-none  hover:bg-zinc-700/10
+                        <DropdownMenuItem onClick={() => onOpen("deleteServer", { server })} className="text-rose-500 focus:outline-none  hover:bg-zinc-700/10
                         dark:hover:bg-zinc-700/50 transition px-3 py-2 text-sm cursor-pointer flex items-center">
                             Delete Server
                             <Trash className="h-4 w-4 ml-auto" />
                         </DropdownMenuItem>
                     )}
                     {!isAdmin && (
-                        <DropdownMenuItem className="text-rose-500 focus:outline-none  hover:bg-zinc-700/10
+                        <DropdownMenuItem onClick={() => onOpen("leaveServer", { server })} className="text-rose-500 focus:outline-none  hover:bg-zinc-700/10
                         dark:hover:bg-zinc-700/50 transition px-3 py-2 text-sm cursor-pointer flex items-center">
                             Leave Server
                             <LogOut className="h-4 w-4 ml-auto" />

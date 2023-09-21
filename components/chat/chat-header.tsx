c2af7ffa8120b7b2ea0a27125e30def24afb3da2
StrukovSergei <strukov.sergei.999@gmail.com>
2023-09-21 21:00:58 +0300

    fixed chat header

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -21,8 +21,8 @@ export const ChatHeader = ({
                 <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
             )}
             <p className="font-semibold text-md text-black dark:text-white">
-                {name}
             </p>
         </div>
+        
     )
 }

aee0ec5a2cff0f80f3e67b64bf5d1d9eddd71c9f
StrukovSergei <strukov.sergei.999@gmail.com>
2023-10-02 16:13:26 +0300

    Video Calls

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,7 +1,10 @@
 import { Hash } from "lucide-react"
+
 import { MobileToggle } from "@/components/mobile-toggle"
 import { UserAvatar } from "@/components/user-avatar"
 import { SocketIndicator } from "@/components/socket-indicator"
+
+import { ChatVideoButton } from "./chat-video-button"
 
 interface ChatHeaderProps {
     serverId: string
@@ -29,7 +32,10 @@ export const ChatHeader = ({
                 {name}
             </p>
             <div className="ml-auto flex items-center">
-                <SocketIndicator/>
+                {type === "conversation" && (
+                    <ChatVideoButton />
+                )}
+                <SocketIndicator />
             </div>
         </div>
     )

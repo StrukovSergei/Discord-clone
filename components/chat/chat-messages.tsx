b9fbeb2f97cdc4dc3fe663b829ed34fe66faa503
StrukovSergei <strukov.sergei.999@gmail.com>
2023-10-02 09:48:36 +0300

    Chat Socket hook

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -5,8 +5,10 @@ import { format } from "date-fns"
 import { Member, Message, Profile } from "@prisma/client"
 import { Loader2, ServerCrash } from "lucide-react"
 
-import { ChatWelcome } from "./chat-welcome"
 import { useChatQuery } from "@/hooks/use-chat-query"
+import { useChatSocket } from "@/hooks/use-chat-socket"
+
+import { ChatWelcome } from "./chat-welcome"
 import { ChatItem } from "./chat-item"
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm"
@@ -41,6 +43,8 @@ export const ChatMessages = ({
     type,
 }: ChatMessagesProps) => {
     const queryKey = `chat:${chatId}`
+    const addKey = `chat:${chatId}:messages`
+    const updateKey = `chat:${chatId}:messages:update`
 
     const {
         data,
@@ -54,6 +58,7 @@ export const ChatMessages = ({
         paramKey,
         paramValue
     })
+    useChatSocket({ queryKey, addKey, updateKey })
 
     if (status === "loading") {
 
